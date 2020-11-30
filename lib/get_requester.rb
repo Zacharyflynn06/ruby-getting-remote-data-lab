7bf1688f2e6797a9b0e15f64322bb369949ba8ab
Zac <zacharyflynn06@gmail.com>
2020-11-30 12:55:30 -0700

    commit tree a12c9234bbff9ae00d94e48b280a5b935e995cc4

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,25 @@
-# Write your code here
+require 'open-uri'
+require 'net/http'
+require 'json'
+require 'pry'
+
+
+class GetRequester
+
+    def initialize(url)
+    end
+
+    
+    def get_response_body
+        uri= URI.parse(URL)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        result = get_response_body
+        JSON.parse(result)
+    end
+
+
+end
